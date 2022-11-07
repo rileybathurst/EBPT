c37c0b5b19c1da3b12f9b6f567a8ee51754c3c51
Riley <riley@rileybathurst.com>
2022-11-07 08:51:35 -0800

    one extra comma breaks things

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -124,7 +124,7 @@ export const SEO = ({
             "paymentAccepted": "${paymentAccepted}",
             "slogan": "${slogan}",
             "image": "${image}",
-            "logo": "${siteUrl}${logo}",
+            "logo": "${siteUrl}${logo}"
           }
         `}
       </Script>
